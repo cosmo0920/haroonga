dd8ba248a62f801324e11b790efd5bb62a9ab038
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-01-13 09:07:04 +0900

    add example for Bindings.Groonga.SimpleAPI

---
+++ example/SimpleAPI/example-SimpleAPI.hs
@@ -0,0 +1,20 @@
+import Bindings.Groonga
+import Bindings.Groonga.SimpleAPI
+
+-- ref: example in http://qiita.com/groonga/items/71b145b37d77bd160bf2
+main :: IO ()
+main = do
+  -- init context
+  ctx <- grn_ctx_init
+  let dbpath = "db/test.db"
+  -- db create
+  _ <- grn_database_create ctx dbpath
+  -- send command
+  let command = "table_create Users TABLE_HASH_KEY ShortText"
+  _ <- grn_execute_command ctx command
+  dump_res <- grn_execute_command ctx "dump"
+  putStrLn dump_res
+  errbuf <- grn_get_errbuf ctx
+  putStrLn errbuf
+  _ <- grn_ctx_fin ctx
+  return ()
